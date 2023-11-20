dd708925dc38cf23cdaba9011a1cc4ac382f0b70
Daniel Pavlicek <dantepawn@users.noreply.github.com>
2023-11-20 12:52:59 +0100

    dockered file
    no multiprocessing ... will be handled by cluster.

---
+++ docker_html2json.py
@@ -0,0 +1,65 @@
+import os
+import json
+from tqdm import tqdm
+from pathlib import Path
+from html2jsonUp import collect
+from pyquery import PyQuery
+import re
+import openstack
+
+
+
+
+def process_html(html_obj, template  , obs_input , obs_output):
+    html_file_content = conn.obs.download_object(html_obj , obs_input)
+    html_file_metadata = conn.obs.get_object_metadata(html_obj , obs_output)
+    
+    html_file_name = html_obj["Key"]
+    json_file_name = str(html_file_name).replace('.html', '.json')
+    
+    #target_folder = os.path.dirname(json_file_name)
+    
+    #if not os.path.exists(target_folder):
+    #    os.makedirs(target_folder)
+    
+    try:
+        data = collect('<html>' + str(html_file_content) + f'<span class="creation-time">{html_file_metadata["LastModified"]}</span></html>', template)
+        #with open(json_file_name, 'w', encoding='utf-8') as f:
+        #    json.dump(data, f, ensure_ascii=False, indent=4)
+        conn.obs.upload_object(name = json_file_name ,container = obs_output , data = json.dumps(data))
+
+    except Exception as e:
+        # write (error, file name) to log file
+        with open('log.txt', 'a') as f:
+            f.write(f'{html_file_name},{e}\n')
+
+def process_html_wrapper(args):
+    # Unpacking arguments and calling process_html function
+    return process_html(*args)
+
+
+def generate_obs_obj(obs_container):
+    for html_obj in conn.obs.objects(obs_container):
+        if html_obj["Key"].endswith('.html'):
+            yield html_obj
+
+if __name__ == '__main__':
+    conn = openstack.connect()
+    
+    #for cont in conn.obs.containers():
+    #    if cont.name == 'companies-test':
+    #        obs_input = cont 
+    #    if cont.name == 'companies-test':
+    #        obs_output = cont       
+    #creation_time = os.path.getmtime(html_file)
+
+    obj_generator = generate_obs_obj('companies-test')
+    os.chdir("/app")
+    
+    with open('/app/html2json/template.json', 'r', encoding='utf-8') as tp:
+        template = json.load(tp)
+
+        for html_obj in obj_generator:
+            process_html(html_obj, template, 'companies-test','companies-json-format')
+        # once the files are processed, upload the log file to OBS
+        conn.obs.upload_object(name = 'log.txt' ,container = 'companies-json-format' , data = open('log.txt', 'rb'))    
